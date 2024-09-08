cca0bc375c3bfd4dccaf2eec8c9a9322453cea49
Yuri <yuri.shimane@gmail.com>
2024-09-07 20:12:13 -0400

    Implemented nonexposure distance function

--- src/quality_metrics.jl
+++ src/quality_metrics.jl
@@ -14,11 +14,27 @@ end
 
 
 """
-Compute non-exposure distance L of tasking solution
+Compute subsequent non-exposure distances L of tasking solution for a single telescope.
+
+```math
+L = sum_{i=1}^{n-1} sum_{j=i}^{n} acos(dot(r_unit_i, r_unit_j)) * Y_i * Y_j
+```
 """
 function get_nonexposure_distance(
     passes::Vector,
     Y::Union{BitVector, Vector{Bool}},
 )
-    return 0
+    Ls = Real[]
+    n = length(passes)
+    for i in 1:n-1
+        r_unit_i = sph2cart(vcat(passes[i].azelf_exposure, [1]))
+        r_unit_j = sph2cart(vcat(passes[i+1].azel0_exposure, [1]))
+        @assert norm(r_unit_j) ≈ 1.0
+        @assert norm(r_unit_i) ≈ 1.0
+        if Y[i] > 0.5 && Y[i+1] > 0.5
+            L = acos(dot(r_unit_i, r_unit_j))
+            push!(Ls, L)
+        end
+    end
+    return sum(Ls), Ls
 end
