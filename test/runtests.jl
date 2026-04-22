c1f5a3a061610e7c0ec14d7970cd82ca32681fbf
Yuri <yuri.shimane@gmail.com>
2026-04-21 22:54:46 -0700

    test updates

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@
 @time begin
     @time "Passes IO" include("test_passes_io.jl")
     @time "Solve single telescope problem" include("test_solve_single.jl")
+    @time "Solve multi telescope problem" include("test_solve_MTTP.jl")
 end
 
 println("Done!")
